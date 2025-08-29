61f0559c7c0bcfe2c80bcd75256a559e3c28ed80
Sanchitjha <sanchitjha8888@gmail.com>
2025-08-30 01:41:05 +0530

    fix: update metadata fields in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'my App',
+  description: 'Created',
+  generator: 'my.app',
 }
 
 export default function RootLayout({
