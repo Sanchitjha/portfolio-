e10e35ee9e0d4a4bbeea03c8da64fbe2bd3e990f
Sanchitjha <sanchitjha8888@gmail.com>
2026-09-16 15:16:41 +0530

    feat: add landing components, configuration files, and social SVG icons

--- web/src/components/landing/Experience.tsx
+++ web/src/components/landing/Experience.tsx
@@ -4,16 +4,16 @@ import React from 'react';
 
 import Container from '../common/Container';
 import SectionHeading from '../common/SectionHeading';
-import { ExperienceCard } from '../experience/ExperienceCard';
+import { ExperienceRow } from '../experience/ExperienceRow';
 import { Button } from '../ui/button';
 
 export default function Experience() {
   return (
     <Container className="mt-20">
-      <SectionHeading subHeading="Featured" heading="Experience" />
-      <div className="mt-4 flex flex-col gap-8">
-        {experiences.slice(0, 2).map((experience: Experience) => (
-          <ExperienceCard key={experience.company} experience={experience} />
+      <SectionHeading heading="Experience" />
+      <div className="mt-6 flex flex-col gap-6">
+        {experiences.slice(0, 3).map((experience: Experience) => (
+          <ExperienceRow key={experience.company} experience={experience} />
         ))}
       </div>
       <div className="mt-8 flex justify-center">
