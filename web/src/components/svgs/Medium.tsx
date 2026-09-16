5cb4fe76b2e199a42593d464b9210248b602c477
Sanchitjha <sanchitjha8888@gmail.com>
2026-09-16 15:17:28 +0530

    feat: add Medium SVG icon component

---
+++ web/src/components/svgs/Medium.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Medium({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path d="M13.54 12a6.8 6.8 0 0 1-6.77 6.82A6.8 6.8 0 0 1 0 12a6.8 6.8 0 0 1 6.77-6.82A6.8 6.8 0 0 1 13.54 12zm7.42 0c0 3.54-1.51 6.42-3.38 6.42S14.2 15.54 14.2 12s1.51-6.42 3.38-6.42S20.96 8.46 20.96 12zM24 12c0 3.17-.53 5.75-1.19 5.75s-1.19-2.58-1.19-5.75.53-5.75 1.19-5.75S24 8.83 24 12z" />
+    </svg>
+  );
+}
